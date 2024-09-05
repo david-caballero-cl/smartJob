546cbfe38f5ce109dc34f4f3675aafbcfce45617
david-caballero-cl <davidcaballero@outlook.com>
2024-09-04 21:11:57 -0400

    implement unit test for token generation

--- src/test/java/com/smartJob/SmartJobApplicationTests.java
+++ src/test/java/com/smartJob/SmartJobApplicationTests.java
@@ -1,13 +1,32 @@
 package com.smartJob;
 
+import com.smartJob.util.JwtUtil;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class SmartJobApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@InjectMocks
+	private JwtUtil jwtUtil;
+
+	private Long expiration = 604800L;
+
+	@BeforeEach
+	void setUp() {
+		MockitoAnnotations.openMocks(this);
+		ReflectionTestUtils.setField(jwtUtil, "expiration", expiration);
 	}
 
+	@Test
+	void testGenerateToken() {
+		String token = jwtUtil.generateToken("testUser");
+		assertNotNull(token);
+	}
 }
